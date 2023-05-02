7bdcbb2f7f74489e80693bac92dc54f518071c50
nkunda-iyeze <shalom.iyeze@gmail.com>
2023-05-02 06:21:44 +0200

    add tasks to db.json file covered

--- src/components/AddTask.jsx
+++ src/components/AddTask.jsx
@@ -1,30 +1,31 @@
-import {addDoc, collection} from "firebase/firestore"
-import { db } from "../firebase";
 import { useState } from "react";
 function AddTask() {
-  const [taskInput,setTaskInput] = useState("")
-  const [dateInput,setDateInput] = useState("");
+  const [title,setTitle] = useState("")
+  const [date,setDate] = useState("");
   const [reminder,setReminder] = useState(false);
   
-  // handleCheckboxChange event
-  function handleCheckboxChange(event) {
-    setReminder(event.target.checked);
-  }
-
-   // create task to firebase db
+  
+   // create task to db.json
    const createTask = async (element) =>{
-    element.preventDefault(element);
-    if(taskInput && dateInput === ''){
-      alert('Please enter date and time');
-      return;
-    }
-    await addDoc(collection(db,'tasks'),{
-      task:taskInput,
-      date:dateInput,
-      reminder:reminder
-    })
-    setTaskInput('');
-    setDateInput('')
+    element.preventDefault()
+    const tasksData = {title,date,reminder};
+
+   try {
+      await fetch("http://localhost:5000/tasks",{
+        method:"POST",
+        headers:{
+          "Content-Type":"application/json"
+        },
+        body:JSON.stringify(tasksData)
+      });
+   } catch(err){
+      console.log("Error ")
+   } finally{
+
+   }
+    setTitle('');
+    setDate('');
+    window.location.reload();
 
   }
   
@@ -34,13 +35,13 @@ function AddTask() {
     <div className="">
     <form action="" className="flex flex-col " onSubmit={createTask}>
       <label htmlFor="task" className="flex justify-start my-1 text-xs font-semibold">Task</label>
-      <input onChange={(e)=>setTaskInput(e.target.value)} value={taskInput} type="text" name="task" className="border border-gray my-1 py-2 px-1 rounded focus:outline-none"/>
+      <input onChange={(e)=>setTitle(e.target.value)} value={title} type="text" name="task" className="border border-gray my-1 py-2 px-1 rounded focus:outline-none" required/>
       <label htmlFor="day&time" className="flex justify-start my-1 text-xs font-semibold">Day & Time</label>
-      <input onChange={(e)=>setDateInput(e.target.value)} value={dateInput}  type="date" name="day&time" className="border border-gray  my-1 py-2 px-1 rounded focus:outline-none"/>
+      <input onChange={(e)=>setDate(e.target.value)} value={date}  type="date" name="day&time" className="border border-gray  my-1 py-2 px-1 rounded focus:outline-none" required/>
       
       <label htmlFor="reminder" className="inline-block justify-start my-1 text-xs font-semibold">Set reminder</label>
       <div className="flex justify-center">
-      <input  type="checkbox" name="reminder" checked={reminder} onChange={handleCheckboxChange}/>
+      <input  type="checkbox" name="reminder" checked={reminder} onChange={(e)=>setReminder(e.target.checked)}/>
       </div>
       <button className="bg-buttonBackground text-white my-3 py-3 rounded-md font-semibold font-sans">Save Task</button>
     </form>
